ba7fb23b9c8a886f4f09949f452c4302271de38f
Red <red.sarouf2014@gmail.com>
2023-04-13 00:31:55 -0400

    TaskLister Mini-Project lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", (e) => {
   // your code here
+
+  const form = document.querySelector("form");
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    console.log(e.target.new_task_description.value);
+    addToTaskList(e.target.new_task_description.value);
+    form.reset();
+  });
 });
+
+function addToTaskList(newTask) {
+  let p = document.createElement("p");
+  let btn = document.createElement("button");
+  btn.textContent = "x";
+  btn.style.backgroundColor = "red";
+  btn.addEventListener("click", deleteTask);
+  p.textContent = `${newTask} `;
+  p.appendChild(btn);
+  console.log(p);
+  document.querySelector("#list").appendChild(p);
+}
+
+function deleteTask(task) {
+  document.querySelector("p").remove();
+}
